a2d81eb7f3ef1c44113e501f2f04ba2f72a5b0e9
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2022-04-16 19:36:27 +0530

    Time: 1 ms (94.88%), Space: 42.1 MB (93.17%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res=0;
-        for(int i=0;i<nums.length;i++){
-            res=res^nums[i];
+        int ele = 0;
+        
+        for(int val : nums) {
+            ele ^= val;
         }
-        return res;
+        
+        return ele;
     }
 }
