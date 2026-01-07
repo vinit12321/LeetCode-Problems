916b506d037923b67d4bf339abef6923da240e91
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-07 13:28:21 +0530

    Time: 4 ms (44.82%), Space: 21.1 MB (5.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor=0
+        for i in range(len(nums)):
+            xor^=nums[i]
+        
+        return xor
+        
