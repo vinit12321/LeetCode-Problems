3a171d1e85fd9aa1a566d292cd872292492c989c
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-07 13:58:57 +0530

    Time: 0 ms (100%), Space: 19.4 MB (8.64%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,17 +1,17 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n==0 or n<0:
-            return False
+        # if n==0 or n<0:
+        #     return False
         
-        while n>1:
-            rem=n%2
-            print("rem",rem)
-            if rem==0:
-                print(n)
-                n=n/2
-            else:
-                print(n)
-                return False
-        return True
+        # while n>1:
+        #     rem=n%2
+        #     print("rem",rem)
+        #     if rem==0:
+        #         n=n/2
+        #     else:
+        #         return False
+        # return True
+
+        return n>0 and (n & (n-1)==0)
             
         
