8a78bf65649866e807916771801646bbf51f104a
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-12 13:01:42 +0530

    Time: 3 ms (34.26%), Space: 19.3 MB (7.44%) - LeetHub

--- LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,13 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         str_list=s.split()
-        new_str=""
-        print(str_list)
-        for i in range(len(str_list)-1,-1,-1):
-            
-            new_str+=str_list[i]
-            if i !=0:
-                new_str+=" "
+        new_str=" ".join(str_list[::-1])        
         return new_str
 
 
