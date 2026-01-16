4896418e34f7afc3ff776987377b4f6faf742f7d
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-16 23:25:10 +0530

    Time: 6 ms (38.34%), Space: 18.7 MB (45.82%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.py
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
-            
+        xor1=0
+        for i in range(1,len(nums)+1):
+            xor1=xor1^i
+        xor2=0
+        for data in nums:
+            xor2=xor2^data
+        missing=xor1^xor2
+        return missing
+
         
