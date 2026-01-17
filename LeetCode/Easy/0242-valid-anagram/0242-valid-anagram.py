47cd782b050ec5203dd024b8bbf44d152fe38b58
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-17 12:34:45 +0530

    Time: 15 ms (42.41%), Space: 19.7 MB (5.41%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,23 @@
-from collections import Counter
 class Solution:
 
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s)==Counter(t)
+        dict1={}
+        dict2={}
+        for i in range(len(s)):
+            if s[i] not in dict1:
+                dict1[s[i]]=1
+            else:
+                dict1[s[i]]+=1
+        
+        for j in range(len(t)):
+            if t[j] in dict1:
+                dict1[t[j]]-=1
+            else:
+                return False
+        
+        for k,v in dict1.items():
+            if v!=0:
+                return False
+        return True
+                
+        
