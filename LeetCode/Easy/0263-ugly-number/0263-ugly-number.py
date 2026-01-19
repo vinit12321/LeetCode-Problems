fc920708d461ad660ec5a6d3e23d1835e647c2c9
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-19 15:42:29 +0530

    Time: 3 ms (15.88%), Space: 19.2 MB (19.27%) - LeetHub

--- LeetCode/Easy/0263-ugly-number/0263-ugly-number.py
+++ LeetCode/Easy/0263-ugly-number/0263-ugly-number.py
@@ -1,10 +1,17 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n<=0:
+        out=[2,3,5]
+        if n <= 0:
             return False
-
-        for p in [2,3,5]:
-            while n%p==0:
-                n=n//p
+ 
+        for num in out:
+            while n%num==0:
+                n=n//num
         
         return n==1
+                
+
+            
+                
+            
+        
