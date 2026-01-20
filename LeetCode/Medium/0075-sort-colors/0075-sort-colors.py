513321bab09f141b0c32deb6585c248cf076dc14
Vinit Dabake <34158995+vinit12321@users.noreply.github.com>
2026-01-20 15:24:25 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.15%) - LeetHub

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.py
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.py
@@ -3,26 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        c1=0
-        c2=0
-        c3=0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                c1+=1
-            elif nums[i]==1:
-                c2+=1
+        
+        low=0
+        mid=0
+        high=len(nums)-1
+
+        while(mid<=high):
+            
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
             else:
-                c3+=1
-        
-        for i in range(c1):
-            nums[i]=0
-        
-        for j in range(c1,c1+c2):
-            nums[j]=1
-
-        for k in range(c1+c2,len(nums)):
-            nums[k]=2
-        
-        return nums
-
-        
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+            
